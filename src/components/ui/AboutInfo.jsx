016191aef68aa365d065f3fe61a3c2aa490cf740
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-03-13 11:48:32 -0700

    set up additional components

---
+++ src/components/ui/AboutInfo.jsx
@@ -0,0 +1,10 @@
+function AboutInfo() {
+  return (
+    <div>
+      <h1>Description Blurb</h1>
+      <p>Blah Blah Blah</p>
+    </div>
+  );
+}
+
+export default AboutInfo;
--- src/components/ui/workCard.jsx
+++ src/components/ui/workCard.jsx
@@ -1,5 +1,13 @@
 function WorkCard() {
-  return <h1>Works</h1>;
+  return (
+    <div>
+      <h1>PROJECT TILES</h1>
+      <div>P1</div>
+      <div>P2</div>
+      <div>P3</div>
+      <div>P4</div>
+    </div>
+  );
 }
 
 export default WorkCard;
