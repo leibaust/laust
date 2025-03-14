d7317efdfc363ca9600603b3d621b51f86d6a2fe
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-03-14 10:49:29 -0700

    implemented framer motion to page transitions

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,17 +1,27 @@
 import AboutInfo from "../components/ui/AboutInfo.jsx";
 import Faq from "../components/ui/Faq.jsx";
 import TechStack from "../components/ui/TechStack.jsx";
+import { motion } from "framer-motion";
 
 function AboutPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-0">
-      <div className="w-full min-h-screen sm:max-w-4/5 max-w-3xl bg-secondary">
-        <h1>About</h1>
-        <TechStack />
-        <AboutInfo />
-        <Faq />
-      </div>
-    </div>
+    <>
+      <motion.div
+        initial={{ y: 300, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -300, opacity: 0 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        <div className="flex items-center justify-center min-h-screen p-0">
+          <div className="w-full min-h-screen sm:max-w-4/5 max-w-3xl bg-secondary">
+            <h1>About</h1>
+            <TechStack />
+            <AboutInfo />
+            <Faq />
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
 }
 
--- src/pages/WorksPage.jsx
+++ src/pages/WorksPage.jsx
@@ -1,12 +1,22 @@
 import WorkCard from "../components/ui/WorkCard";
+import { motion } from "framer-motion";
 
 function WorksPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-0">
-      <div className="w-full min-h-screen sm:max-w-4/5 max-w-3xl bg-secondary flex items-center justify-center">
-        <WorkCard />
-      </div>
-    </div>
+    <>
+      <motion.div
+        initial={{ y: 300, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -300, opacity: 0 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        <div className="flex items-center justify-center min-h-screen p-0">
+          <div className="w-full min-h-screen sm:max-w-4/5 max-w-3xl bg-secondary flex items-center justify-center">
+            <WorkCard />
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
 }
 
