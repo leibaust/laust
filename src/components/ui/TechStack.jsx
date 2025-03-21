4a9b924bf8bcea4fd29b317056fa8f822369156f
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-03-21 14:52:28 -0700

    pre-animatepresence

--- src/components/ui/TechStack.jsx
+++ src/components/ui/TechStack.jsx
@@ -24,9 +24,11 @@ function TechStack() {
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col items-center justify-center">
-        <h2 className="text-4xl rotate-270 sm:rotate-0">STACK</h2>
+        <h2 className="hidden lg:block text-4xl rotate-270 sm:rotate-0">
+          STACK
+        </h2>
       </div>
-      <div className="grid grid-cols-4 sm:grid-cols-9 gap-4 ml-4 my-auto">
+      <div className="grid grid-cols-6 lg:grid-cols-9 gap-4 ml-4 my-auto">
         <FaHtml5 className="text-2xl" />
         <FaCss3Alt className="text-2xl" />
         <IoLogoJavascript className="text-2xl" />
